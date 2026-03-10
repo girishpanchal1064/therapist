605b2e3b58ee43487190defac9ff23ae51b238e3
girishpanchal1064 <girishpanchal1064@gmail.com>
2026-03-10 15:46:18 +0530

    fixes
    fixes

--- resources/views/layouts/sections/styles.blade.php
+++ resources/views/layouts/sections/styles.blade.php
@@ -4,8 +4,6 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap" rel="stylesheet">
-
-@vite(['resources/assets/vendor/fonts/remixicon/remixicon.scss'])
 <!-- Core CSS -->
 @vite([
   'resources/assets/vendor/scss/core.scss',
