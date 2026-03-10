dd09f31dbfbb0b9942093199492bcbf617e84d10
girishpanchal1064 <girishpanchal1064@gmail.com>
2026-03-10 15:48:36 +0530

    fixes
    fixes

--- resources/views/layouts/sections/styles.blade.php
+++ resources/views/layouts/sections/styles.blade.php
@@ -4,15 +4,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&ampdisplay=swap" rel="stylesheet">
-<!-- Core CSS -->
-@vite([
-  'resources/assets/vendor/scss/core.scss',
-  'resources/assets/vendor/scss/theme-default.scss',
-  'resources/assets/css/demo.css'
-])
-
-<!-- Vendor Styles -->
-@vite(['resources/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.scss'])
+<!-- Core & Vendor CSS are bundled via resources/css/app.css -->
 @yield('vendor-style')
 
 <!-- Page Styles -->
